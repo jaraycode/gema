c1330e55d7bcf16ab16f297a63e2e9b76d7dc77f
jaraycode <jonasaray12@gmail.com>
2025-05-28 15:56:14 -0400

    feat: models, seeders and form request for Departmet, Equipment, Personel and Location created but not implemented

---
+++ database/seeders/PersonelSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PersonelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
