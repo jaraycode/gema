853973c219e226f9f462d2a751c7aec8fd1ae300
jaraycode <jonasaray12@gmail.com>
2025-05-29 00:23:39 -0400

    feat: department now have a seeder, also it's Model is configured

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DepartmentSeeder extends Seeder
 {
@@ -12,6 +14,37 @@ class DepartmentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Department::insert([
+            [
+                'name' => 'Grupo de mantenimiento de refrigeración',
+                'code' => 'SGMREF',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Grupo de mantenimiento de infraestructura',
+                'code' => 'SGMINF',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Grupo de mantenimiento eléctrico',
+                'code' => 'SGMELE',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Grupo de mantenimiento de logística',
+                'code' => 'SGMLOG',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Grupo de mantenimiento mecánico',
+                'code' => 'SGMMÉC',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
     }
 }
