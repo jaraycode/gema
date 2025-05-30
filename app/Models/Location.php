5b37b5aeb3558298b4043363722045c4edb05d25
jaraycode <jonasaray12@gmail.com>
2025-05-30 11:41:08 -0400

    feat: following seeders for Location

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    protected $table = 'location';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name', 'code', 'level_min', 'level_max'];
 }
--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Location;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,71 @@ class LocationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Location::insert(values: [
+            [
+                'name' => 'Módulo 2',
+                'code' => 'M2',
+                'level_min' => 1,
+                'level_max' => 1,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Planta baja',
+                'code' => 'PB',
+                'level_min' => 2,
+                'level_max' => 2,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => '',
+                'code' => '',
+                'level_min' => 1,
+                'level_max' => 1,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => '',
+                'code' => '',
+                'level_min' => 1,
+                'level_max' => 1,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => '',
+                'code' => '',
+                'level_min' => 1,
+                'level_max' => 1,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => '',
+                'code' => '',
+                'level_min' => 1,
+                'level_max' => 1,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => '',
+                'code' => '',
+                'level_min' => 1,
+                'level_max' => 1,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => '',
+                'code' => '',
+                'level_min' => 1,
+                'level_max' => 1,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+        ]);
     }
 }
