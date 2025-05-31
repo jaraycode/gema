11ba27bb916c9f16f14218d7432f2fd272b1f01c
jaraycode <jonasaray12@gmail.com>
2025-05-31 15:22:54 -0400

    fix: use HEAD component from Inertia to indicate name of the tab

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,9 +1,12 @@
 import DashboardLayout from '@/layouts/dashboard-layout';
+import { Head } from '@inertiajs/react';
 
 export default function Dashboard({ name }: { name: string }) {
     // Punto de entrada del dashboard, a partir de aqui es qeu se mandan las props
     return (
         <DashboardLayout>
+            <Head title="Dashboard" />
+
             <h1>Usando custom layout {name}</h1>
         </DashboardLayout>
     );
