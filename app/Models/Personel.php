dae1f1fc82b4395023a1d7c84babeb069e129f70
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2025-06-02 22:06:05 -0400

    feat(backend): fillable fields and relation on personel model

--- app/Models/Personel.php
+++ app/Models/Personel.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Personel extends Model
 {
-    //
+
+    use HasFactory;
+    protected $table = 'personel';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        "email",
+        "username",
+        "password",
+        "phone_number",
+        "first_name",
+        "second_name",
+        "last_name",
+        "second_last_name"
+    ];
+
+    public function personel(): BelongsToMany
+    {
+        return $this->belongsToMany(WorkGroup::class);
+    }
 }
