1e34ea2bbd7d519b4ca27579163958c9dca6fa66
Yisustxz <jesusoortiz2410@gmail.com>
2025-06-08 21:37:17 -0400

    fix: cambios a estilado de la tabla

--- resources/js/components/DepartmentTable.tsx
+++ resources/js/components/DepartmentTable.tsx
@@ -1,168 +1,139 @@
-"use client"
+'use client';
 
-import * as React from "react"
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  ColumnDef,
-  ColumnFiltersState,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  SortingState,
-  useReactTable,
-} from "@tanstack/react-table"
-import { Input } from "@/components/ui/input"
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-import { Button } from "@/components/ui/button"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFileExcel } from '@fortawesome/free-solid-svg-icons'
-import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons"
+    ColumnDef,
+    ColumnFiltersState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    SortingState,
+    useReactTable,
+} from '@tanstack/react-table';
+import * as React from 'react';
 
 export type Department = {
-  id: string
-  name: string
-}
+    id: string;
+    name: string;
+};
 
 export const columns: ColumnDef<Department>[] = [
-  {
-    accessorKey: "id", // Cambiado de "codigo" a "id"
-    header: () => <div className="text-center">Código</div>,
-    cell: ({ row }) => <div className="text-center">{row.getValue("id")}</div>,
-  },
-  {
-    accessorKey: "name",
-    header: () => <div className="text-center">Nombre</div>,
-    cell: ({ row }) => <div className="text-center">{row.getValue("name")}</div>,
-  },
-]
+    {
+        accessorKey: 'id', // Cambiado de "codigo" a "id"
+        header: () => <div className="text-center">Código</div>,
+        cell: ({ row }) => <div className="text-center">{row.getValue('id')}</div>,
+    },
+    {
+        accessorKey: 'name',
+        header: () => <div className="text-center">Nombre</div>,
+        cell: ({ row }) => <div className="text-center">{row.getValue('name')}</div>,
+    },
+];
 
 export function DepartmentTable({ data }: { data: Department[] }) {
-  const [sorting, setSorting] = React.useState<SortingState>([])
-  const [globalFilter, setGlobalFilter] = React.useState("")
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
+    const [sorting, setSorting] = React.useState<SortingState>([]);
+    const [globalFilter, setGlobalFilter] = React.useState('');
+    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
 
-  const globalFilterFn = (row: any, _columnId: string, filterValue: string) => {
-    const search = filterValue.toLowerCase()
+    const globalFilterFn = (row: any, _columnId: string, filterValue: string) => {
+        const search = filterValue.toLowerCase();
+        return row.getValue('id')?.toString().toLowerCase().includes(search) || row.getValue('name')?.toLowerCase().includes(search);
+    };
+
+    const table = useReactTable({
+        data,
+        columns,
+        onSortingChange: setSorting,
+        onColumnFiltersChange: setColumnFilters,
+        onGlobalFilterChange: setGlobalFilter,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        globalFilterFn,
+        state: {
+            sorting,
+            columnFilters,
+            globalFilter,
+        },
+        initialState: {
+            pagination: {
+                pageSize: 10,
+            },
+        },
+    });
+
     return (
-      row.getValue("id")?.toString().toLowerCase().includes(search) ||
-      row.getValue("name")?.toLowerCase().includes(search)
-    )
-  }
-
-  const table = useReactTable({
-    data,
-    columns,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
-    onGlobalFilterChange: setGlobalFilter,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    globalFilterFn,
-    state: {
-      sorting,
-      columnFilters,
-      globalFilter
-    },
-    initialState: {
-      pagination: {
-        pageSize: 10, 
-      },
-    },
-  })
-
-  return (
-    <div className="w-full">
-      <div className="flex items-center py-4">
-        <Input
-          placeholder="Buscar Departamento..."
-          value={globalFilter}
-          onChange={(e) => setGlobalFilter(e.target.value)}
-          className="max-w-sm border border-gray-10 hover:border-gray-10"
-        />
-        <a href="/department/new" className="flex items-center justify-center p-3 bg-[#1E9483] text-white rounded-[20px] ml-auto mr-10 transition duration-200 hover:shadow hover:shadow-[#1E9483] w-55">
-            Agregar Departamento
-        </a>
-      </div>
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
+        <div className="w-full">
+            <div className="flex items-center py-4">
+                <Input
+                    placeholder="Buscar Departamento..."
+                    value={globalFilter}
+                    onChange={(e) => setGlobalFilter(e.target.value)}
+                    className="border-gray-10 hover:border-gray-10 max-w-sm border"
+                />
+                <a
+                    href="/department/new"
+                    className="mr-10 ml-auto flex w-55 items-center justify-center rounded-[20px] bg-[#1E9483] p-3 text-white transition duration-200 hover:shadow hover:shadow-[#1E9483]"
+                >
+                    Agregar Departamento
+                </a>
+            </div>
+            <div className="rounded-md border">
+                <Table className="border-separate border-spacing-y-6 border-gray-200">
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                                    </TableHead>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow key={row.id}>
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={columns.length} className="h-24 text-center">
+                                    Sin resultados.
+                                </TableCell>
+                            </TableRow>
                         )}
-                  </TableHead>
-                ))}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow key={row.id}>
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={columns.length} className="h-24 text-center">
-                  Sin resultados.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
+                    </TableBody>
+                </Table>
+            </div>
 
-      <div className="flex items-center justify-between py-4">
-        <div className="text-muted-foreground text-sm">
-          {table.getFilteredRowModel().rows.length} fila(s) mostradas.
+            <div className="flex items-center justify-between py-4">
+                <div className="text-muted-foreground text-sm">{table.getFilteredRowModel().rows.length} fila(s) mostradas.</div>
+                <div className="space-x-2">
+                    <div className="flex items-center gap-2">
+                        <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+                            <FontAwesomeIcon icon={faChevronLeft} />
+                        </Button>
+
+                        <span className="px-2 text-sm font-medium">{table.getState().pagination.pageIndex + 1}</span>
+
+                        <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+                            <FontAwesomeIcon icon={faChevronRight} />
+                        </Button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="space-x-2">
-          <div className="flex items-center gap-2">
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-            >
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </Button>
-
-            <span className="text-sm font-medium px-2">
-              {table.getState().pagination.pageIndex + 1}
-            </span>
-
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => table.nextPage()}
-              disabled={!table.getCanNextPage()}
-            >
-              <FontAwesomeIcon icon={faChevronRight} />
-            </Button>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    );
 }
