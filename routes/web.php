53b708604ef6bc02260ce9fa92230b342b313bd8
jaraycode <jonasaray12@gmail.com>
2025-06-12 15:52:31 -0400

    fix: rutas de ubicacion

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
-use App\Http\Controllers\Location\LocationController;
 use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->get('/', [DashboardController::class, 'index'])->name('home');
 
-require __DIR__ . '/locationRoutes.php';
+require __DIR__ . '/LocationRoutes.php';
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
 require __DIR__ . '/PersonelRoutes.php';
