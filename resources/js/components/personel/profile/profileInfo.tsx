500cbe70632258893d5f5fe2c979638b913af886
Yisustxz <jesusoortiz2410@gmail.com>
2025-06-19 23:38:06 -0400

    fix: arreglos pequeños de estilo

--- resources/js/components/personel/profile/profileInfo.tsx
+++ resources/js/components/personel/profile/profileInfo.tsx
@@ -16,7 +16,7 @@ interface ProfileInfoProps {
 export function ProfileInfo({ data }: ProfileInfoProps) {
     return (
         <div className="bg-card text-card-foreground rounded-xl border shadow-sm">
-            <div className="p-8">
+            <div className="p-10">
                 <div className="mb-8 flex flex-col items-center gap-6">
                     <div className="flex flex-col items-center gap-4">
                         <h1 className="text-2xl font-bold">Perfil</h1>
@@ -31,49 +31,46 @@ export function ProfileInfo({ data }: ProfileInfoProps) {
                 </div>
 
                 <hr className="my-6 border-t border-gray-200" />
-                 
 
-                <div className="mt-8 flex justify-between grid-cols-2 pr-5">
+                <div className="mt-8 flex grid-cols-2 justify-between pr-10">
                     <div className="flex gap-2">
                         <FiUser className="size-6" />
                         <h2 className="mb-6 text-xl font-semibold">Información Personal</h2>
                     </div>
-                     <Button className="px-8 py-2 text-base rounded-xl bg-[#1e9483] text-white hover:bg-[#1e9483]/90 shadow-sm">
+                    <Button className="rounded-xl bg-[#1e9483] px-12 py-6 text-base text-white shadow-sm hover:bg-[#1e9483]/90">
                         <FiPlus />
                         Editar Perfil
-                     </Button>
+                    </Button>
                 </div>
-               
-                <div className="grid grid-cols-2 gap-y-1 md:grid-cols-2 md:gap-x-10 md:gap-y-8 mt-5">
+
+                <div className="mt-5 grid grid-cols-2 gap-y-1 md:grid-cols-2 md:gap-x-10 md:gap-y-8">
                     <div className="space-y-3">
                         <div className="font-medium text-gray-500">Nombre</div>
-                        <div className="p-3 text-lg bg-white  rounded-xl border-gray-300 border-1 shadow-sm w-80">{data.nombre}</div>
+                        <div className="w-80 rounded-xl border-1 border-gray-300 bg-white p-3 text-lg shadow-sm">{data.nombre}</div>
                     </div>
                     <div className="space-y-3">
                         <div className="font-medium text-gray-500">Cedula</div>
-                        <div className="p-3 text-lg bg-white  rounded-xl border-gray-300 border-1 shadow-sm w-80">{data.cedula}</div>
+                        <div className="w-80 rounded-xl border-1 border-gray-300 bg-white p-3 text-lg shadow-sm">{data.cedula}</div>
                     </div>
 
                     <div className="space-y-3">
                         <div className="font-medium text-gray-500">Teléfono</div>
-                        <div className="p-3 text-lg bg-white  rounded-xl border-gray-300 border-1 shadow-sm w-80">{data.telefono}</div>
+                        <div className="w-80 rounded-xl border-1 border-gray-300 bg-white p-3 text-lg shadow-sm">{data.telefono}</div>
                     </div>
                     <div className="space-y-3">
                         <div className="font-medium text-gray-500">Correo Electrónico</div>
-                        <div className="p-3 text-lg bg-white  rounded-xl border-gray-300 border-1 shadow-sm w-80">{data.email}</div>
+                        <div className="w-80 rounded-xl border-1 border-gray-300 bg-white p-3 text-lg shadow-sm">{data.email}</div>
                     </div>
 
                     <div className="space-y-3">
                         <div className="font-medium text-gray-500">Cargo</div>
-                        <div className="p-3 text-lg bg-white  rounded-xl border-gray-300 border-1 shadow-sm w-80">{data.cargo}</div>
+                        <div className="w-80 rounded-xl border-1 border-gray-300 bg-white p-3 text-lg shadow-sm">{data.cargo}</div>
                     </div>
                     <div className="space-y-3">
                         <div className="font-medium text-gray-500">Departamento</div>
-                        <div className="p-3 text-lg bg-white  rounded-xl border-gray-300 border-1 shadow-sm w-80">{data.departamento}</div>
+                        <div className="w-80 rounded-xl border-1 border-gray-300 bg-white p-3 text-lg shadow-sm">{data.departamento}</div>
                     </div>
                 </div>
-
-               
             </div>
         </div>
     );
