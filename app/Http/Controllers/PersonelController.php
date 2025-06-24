9abab8c6ebfce8d84e158e999019e6231833735f
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2025-06-23 22:39:13 -0400

    feat(backend): personnel to service

--- app/Http/Controllers/PersonelController.php
+++ app/Http/Controllers/PersonelController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Http\Requests\Core\Personel\StorePersonelRequest;
 use App\Http\Requests\Core\Personel\UpdatePersonelRequest;
 use App\Models\Personel;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Hash;
 use App\Service\Personel\PersonelService;
+use Inertia\Response;
 
 class PersonelController extends Controller
 {
@@ -18,7 +21,7 @@ class PersonelController extends Controller
   public function index()
   {
     $personelProps = $this->personelService->getMenu();
-    $personels = Personel::paginate(10);
+    $personels = PersonelService::getAllPersonnel();
 
     return Inertia::render('persona/personel', array_merge($personelProps, [
       'personels' => $personels
@@ -45,33 +48,39 @@ class PersonelController extends Controller
    */
   public function store(StorePersonelRequest $request)
   {
-    $password = $request->get('password');
-    $hashedPassword = Hash::make($password);
-    $personel = Personel::create([
-      "email" => $request->get("email"),
-      "username" => $request->get("username"),
-      "password" => $hashedPassword,
-      "phone_number" => $request->get("phone_number"),
-      "first_name" => $request->get("first_name"),
-      "second_name" => $request->get("second_name"),
-      "last_name" => $request->get("last_name"),
-      "second_last_name" => $request->get("second_last_name"),
-    ]);
+      $password = $request->get('password');
+      $hashedPassword = Hash::make($password);
+      $personnel = [
+          "email" => $request->get("email"),
+          "username" => $request->get("username"),
+          "password" => $hashedPassword,
+          "phone_number" => $request->get("phone_number"),
+          "first_name" => $request->get("first_name"),
+          "second_name" => $request->get("second_name"),
+          "last_name" => $request->get("last_name"),
+          "second_last_name" => $request->get("second_last_name"),
+      ];
 
-    //return redirect("personels.index")
-    return response()->json($personel, 201);
+      $response = $this->personelService->storePersonnel($personnel);
+      return $response;
   }
 
   /**
    * Display the specified resource.
    */
-  public function show()
+  public function show(string $id)
   {
-    /*  $personel = Personel::all()->findOrFail($id);
-    return response()->json($personel, 200); */
-    $dashboardProps = $this->dashboardService->getMenu();
+      try {
+          $response = $this->personelService->getPersonnel(id: intval(value: $id));
 
-    return Inertia::render('persona/profile', array_merge($dashboardProps, []));
+          $dashboardProps = $this->dashboardService->getMenu();
+
+          return Inertia::render('personel/profile', array_merge($dashboardProps, [
+              'personel' => $response
+          ]));
+      } catch (Exception $e) {
+          return redirect()->back()->with(key: "error", value: $e->getMessage());
+      }
   }
 
   /**
@@ -79,8 +88,20 @@ class PersonelController extends Controller
    */
   public function update(UpdatePersonelRequest $request, string $id)
   {
-    $personel = Personel::all()->findOrFail($id);
-    $personel->update($request->all());
+      return $this->personelService->updatePersonnel(id: intval(value: $id), personnel: $request->validated());
+  }
+
+    /**
+     * Renders the update screen of the specified resource in storage.
+     */
+  public function edit(string $id): RedirectResponse | Response {
+      try {
+          $personnel = $this->personelService->getPersonnel(id: intval(value: $id));
+          $dashboardProps = $this->personelService->getMenu();
+          return Inertia::render(component: 'personel/edit', props: array_merge($dashboardProps, ['data' => $personnel]));
+      } catch (Exception $e) {
+          return redirect()->back()->with(key: 'error', value: $e->getMessage());
+      }
   }
 
   /**
@@ -88,9 +109,6 @@ class PersonelController extends Controller
    */
   public function destroy(string $id)
   {
-    $personel = Personel::all()->findOrFail($id);
-    $personel->delete();
-
-    return response()->json(null, 204);
+      return $this->personelService->deletePersonnel(id: $id);
   }
 }
--- app/Service/Personel/PersonelService.php
+++ app/Service/Personel/PersonelService.php
@@ -2,7 +2,12 @@
 
 namespace App\Service\Personel;
 
+use App\Models\Personel;
 use App\Repository\Core\SidebarRepository;
+use Exception;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Log;
+use PhpParser\Builder;
 
 class PersonelService
 {
@@ -14,5 +19,57 @@ class PersonelService
     public function getMenu(): array
     {
         return setActiveRoute(menu: $this->menu->getSidebarMenu(), title: $this->title);
+    }
+
+    public function getAllPersonnel(): array
+    {
+        return Personel::where(column: 'delete_at')->paginate(perPage: Personel::count())->through(callback: function ($value): mixed {
+            $value->level = castLevel(index: $value->level - 1);
+            return $value;
+        });
+    }
+
+    public function getPersonnel(int $id): Builder|Personel {
+        $personnel = Personel::where(column: 'delete_at')->find($id);
+
+        if (!$personnel) {
+            throw new \Exception(message: 'Personal no encontrado');
+        }
+
+        return $personnel;
+    }
+
+    public function storePersonnel(array $personnel): RedirectResponse
+    {
+        try {
+            $response = Personel::create(attributes: $personnel);
+            if ($response) return redirect()->route(route: 'personel.index')->with(key: 'success', value: 'Personal creado exitosamente');
+            return redirect()->back()->with(key: 'error', value: 'No se pudo crear el personal. Intente nuevamente!');
+        } catch (Exception $e) {
+            Log::error(message: 'Creación de Personal ' . $e->getMessage());
+            throw new \Exception(message: "Error al guardar Personal: " . $e->getMessage());
+        }
+    }
+
+    public function updatePersonnel(int $id, array $personnel): RedirectResponse {
+        try {
+            $response = Personel::where(column: 'id', operator: '=', value: $id)->update($personnel);
+            if ($response > 0) return redirect()->route(route: 'personel.index')->with(key: 'success', value: 'Personal actualizado exitosamente');
+            return redirect()->back()->with(key: 'error', value: 'No se pudo actualizar el Personal. Intente nuevamente!');
+        } catch (Exception $e) {
+            Log::error(message: 'Actualización de Personal ' . $e->getMessage());
+            throw new Exception(message: "Error al actualizar Personal: " . $e->getMessage());
+        }
+    }
+
+    public function deletePersonnel(int $id): RedirectResponse {
+        try {
+            $response = Personel::where(column: 'id', operator: '=', value: $id)->update(['delete_at' => now()]);
+            if ($response > 0) return redirect()->route(route: 'personel.index')->with(key: 'success', value: 'Personel eliminado exitosamente');
+            return redirect()->back()->with(key: 'error', value: 'No se pudo eliminar el Personal. Intente nuevamente!');
+        } catch (Exception $e) {
+            Log::error(message: 'Borrado de Personal ' . $e->getMessage());
+            throw new Exception(message: "Error al borrar Personal: " . $e->getMessage());
+        }
     }
 }
